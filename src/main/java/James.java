eac3ff361eb85eaceb46066e3828b6baf0213229
rahul <rahul.vijay.0406@gmail.com>
2025-08-21 19:59:08 +0800

    Add list functionality

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 public class James {
+    public static void displayList(String[] arr) {
+        int count = 1;
+        for (String str: arr) {
+            if (str != null) {
+                System.out.println("<" + count + "> " + str);
+            }
+            count++;
+        }
+    }
 
     public static void main(String[] args) {
+        String[] store = new String[100];
+        int size = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Hey There! James at your service. \n" +
                 "How can I help you today?");
@@ -11,7 +22,15 @@ public class James {
             String query = input.nextLine().trim();
             System.out.println("--------------------------------------------------------------");
             if (query.equalsIgnoreCase("bye")) break;
-            System.out.println("output: " + query);
+            if (query.equalsIgnoreCase("list")) {
+                System.out.print("output:\n");
+                System.out.println("Item count: " + size);
+                James.displayList(store);
+            } else {
+                System.out.println("output:\n" + "added: " + query);
+                store[size] = query;
+                size++;
+            }
             System.out.println("--------------------------------------------------------------");
         }
         System.out.println("Bye, feel free to ask me anything anytime!");
