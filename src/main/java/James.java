cbd4984d0a195ce69e1e91bbc6abf82c7b830b03
rahul <rahul.vijay.0406@gmail.com>
2025-08-21 19:19:29 +0800

    Add echo functionality

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class James {
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hey There! James at your service. \n" +
                 "How can I help you today?");
         System.out.println("--------------------------------------------------------------");
+        while (true) {
+            System.out.println("input: ");
+            String query = input.nextLine().trim();
+            System.out.println("--------------------------------------------------------------");
+            if (query.equalsIgnoreCase("bye")) break;
+            System.out.println("output: " + query);
+            System.out.println("--------------------------------------------------------------");
+        }
         System.out.println("Bye, feel free to ask me anything anytime!");
     }
 }
+
+
+
