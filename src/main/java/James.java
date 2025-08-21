2351565b5a79008469edb262c3702c1a7131565d
rahul <rahul.vijay.0406@gmail.com>
2025-08-21 18:28:31 +0800

    Add greeting

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -2,7 +2,7 @@ public class James {
     public static void main(String[] args) {
         System.out.println("Hey There! James at your service. \n" +
                 "How can I help you today?");
-        System.out.println("-----------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------");
         System.out.println("Bye, feel free to ask me anything anytime!");
     }
 }
