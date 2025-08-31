3b01231abccc57d856f4f9a64d97c790d01e68bf
rahul <rahul.vijay.0406@gmail.com>
2025-08-31 18:36:24 +0800

    Fix more merge conflict mistakes

--- src/main/java/james/TaskList.java
+++ src/main/java/james/TaskList.java
@@ -7,29 +7,58 @@ import java.util.stream.Stream;
 public class TaskList {
     private ArrayList<Task> tasks;
     private int size;
+
+    /**
+     * Constructs a TaskList using an existing list of tasks.
+     *
+     * @param t ArrayList of Task objects to initialize the task list.
+     */
     public TaskList(ArrayList<Task> t) {
         this.tasks = t;
         this.size = t.size();
     }
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
         this.size = 0;
     }
 
+    /**
+     * Adds a new task to the task list and increments the size counter.
+     *
+     * @param t Task object to be added.
+     */
     public void add(Task t){
         tasks.add(t);
         this.size++;
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     *
+     * @param n Index of the task to retrieve (0-based).
+     * @return Task object at the given index.
+     */
     public Task get(int n) {
         return this.tasks.get(n);
     }
 
+    /**
+     * Returns the entire list of tasks.
+     *
+     * @return ArrayList containing all Task objects.
+     */
     public ArrayList<Task> getTasks() {
         return this.tasks;
     }
 
+    /**
+     * Displays all tasks in the list with their corresponding index.
+     * Skips null entries if any exist.
+     */
     public void displayTask() {
         int count = 1;
         for (Task tsk: this.tasks) {
@@ -40,6 +69,13 @@ public class TaskList {
         }
     }
 
+    /**
+     * Deletes a task based on the input query string.
+     * Assumes the query is in the format "delete <Index>".
+     *
+     * @param query String containing the delete command and task number.
+     * @return Task object that was removed.
+     */
     public Task deleteTask(String query) {
         String[] words = query.split(" ");
         int taskNo = Integer.parseInt(words[1].trim()) - 1;
@@ -48,6 +84,12 @@ public class TaskList {
         return this.tasks.remove(taskNo);
     }
 
+    /**
+     * Displays tasks that contain a specific keyword in their description.
+     * Assumes the query is in the format "find <searchItem>".
+     *
+     * @param query String containing the search command and keyword.
+     */
     public void displayTasksWithString(String query) {
         String searchItem = query.split(" ", 2)[1];
         //System.out.println(searchItem);
@@ -66,6 +108,12 @@ public class TaskList {
         }
 
     }
+
+    /**
+     * Returns the current number of tasks in the list.
+     *
+     * @return Integer representing the size of the task list.
+     */
     public int getSize() {
         return this.size;
     }
